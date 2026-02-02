4908dd0680f0e652e881a8015580a3dd7ed9f577
Soroush Dianaty <112230234+soroushdty@users.noreply.github.com>
2026-02-02 02:24:46 -0700

    Import tqdm from the tqdm module

--- funcs/compute_embeddings.py
+++ funcs/compute_embeddings.py
@@ -4,7 +4,7 @@ import pandas as pd
 import torch
 import numpy as np
 import pandas as pd
-import tqdm
+from tqdm import tqdm
 # import logging
 from pathlib import Path
 from transformers import AutoTokenizer, AutoModel
