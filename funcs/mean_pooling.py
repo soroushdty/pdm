41478b223ad06f0df7a287e26b31bcb46b59f6dc
Soroush Dianaty <112230234+soroushdty@users.noreply.github.com>
2026-02-02 02:32:13 -0700

    Add PyTorch import for mean pooling function

--- funcs/mean_pooling.py
+++ funcs/mean_pooling.py
@@ -1,3 +1,4 @@
+import torch
 def mean_pooling(last_hidden_state, attention_mask):
     """Mean pooling with attention mask"""
     mask = attention_mask.unsqueeze(-1).expand(last_hidden_state.size()).float()
