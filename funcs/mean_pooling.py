22780abf69d9cb83ae21d007f76eacd38232733a
Soroush Dianaty <112230234+soroushdty@users.noreply.github.com>
2026-02-02 01:01:07 -0700

    Implement mean pooling function with attention mask

---
+++ funcs/mean_pooling.py
@@ -0,0 +1,6 @@
+def mean_pooling(last_hidden_state, attention_mask):
+    """Mean pooling with attention mask"""
+    mask = attention_mask.unsqueeze(-1).expand(last_hidden_state.size()).float()
+    summed = torch.sum(last_hidden_state * mask, dim=1)
+    counts = mask.sum(dim=1).clamp(min=1e-9)
+    return summed / counts
